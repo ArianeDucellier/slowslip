0a3987ef7e78440a829707236bb3125e7e0e9c3d
ArianeDucellier <ariane.ducellier.pro@gmail.com>
2021-07-24 18:07:12 -0700

    Write synthetics part

--- src/synthetics.py
+++ src/synthetics.py
@@ -27,8 +27,15 @@ np.random.seed(0)
 
 # Loop on time between events
 for timestep in timesteps:
+
+    # Start figure
+    params = {'xtick.labelsize':20,
+              'ytick.labelsize':20}
+    pylab.rcParams.update(params)   
+    fig = plt.figure(1, figsize=(5 * len(durations), 3 * (J + 3)))
+
     # Loop on duration
-    for duration in durations:
+    for i, duration in enumerate(durations):
         # Create displacements vectors
         part1 = np.arange(0, timestep - duration) / (timestep - duration)
         part2 = (timestep - np.arange(timestep - duration, timestep)) / duration
@@ -39,29 +46,36 @@ for timestep in timesteps:
         (D, S) = MODWT.get_DS(disp, W, name, J)
         maxD = max([np.max(Dj) for Dj in D])
         minD = min([np.min(Dj) for Dj in D])
-        # Plot MODWT
-        params = {'xtick.labelsize':24,
-                  'ytick.labelsize':24}
-        pylab.rcParams.update(params)   
-        fig = plt.figure(1, figsize=(15, 5 * (J + 3)))
         # Plot data
-        plt.subplot2grid((J + 2, 1), (J + 1, 0))
+        ax = plt.subplot2grid((J + 2, len(durations)), (J + 1, i))
         plt.plot(time, disp, 'k', label='Data')
+        plt.xlabel('Time (days)', fontsize=20)
         plt.ylim(-2.0, 2.0)
-        plt.legend(loc=3, fontsize=14)
+        plt.legend(loc=3, fontsize=20)
+        if i != 0:
+            ax.axes.yaxis.set_ticks([])
         # Plot details
         for j in range(0, J):
-            plt.subplot2grid((J + 2, 1), (J - j, 0))
+            ax = plt.subplot2grid((J + 2, len(durations)), (J - j, i))
             plt.plot(time, D[j], 'k', label='D' + str(j + 1))
             plt.ylim(minD, maxD)
-            plt.legend(loc=3, fontsize=14)
+            plt.legend(loc=3, fontsize=20)
+            ax.axes.xaxis.set_ticks([])
+            if i != 0:
+                ax.axes.yaxis.set_ticks([])
         # Plot smooth
-        plt.subplot2grid((J + 2, 1), (0, 0))
+        ax = plt.subplot2grid((J + 2, len(durations)), (0, i))
         plt.plot(time, S[J], 'k', label='S' + str(J))
         plt.ylim(-2.0, 2.0)
-        plt.legend(loc=3, fontsize=14)
-        # Save figure
-        title = 'Time between events = ' + str(timestep) + ' - Duration of events = ' + str(duration)
-        plt.suptitle(title, fontsize=30)
-        plt.savefig('synthetics/' + str(timestep) + '_' + str(duration) + '_DS.eps', format='eps')
-        plt.close(1)
+        plt.legend(loc=3, fontsize=20)
+        ax.axes.xaxis.set_ticks([])
+        if i != 0:
+            ax.axes.yaxis.set_ticks([])
+        title = 'Duration of event = ' + str(duration) + ' days'
+        plt.title(title, fontsize=20)
+
+    # Save figure
+    title = 'Time between events = ' + str(timestep)
+    plt.suptitle(title, fontsize=30)
+    plt.savefig('synthetics/' + str(timestep) + '_DS.pdf', format='pdf')
+    plt.close(1)
